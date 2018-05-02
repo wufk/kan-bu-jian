cee952e012af4ef97c86d4752c9e54a4f62891e5
YizhouLuo <986732031@qq.com>
2018-05-01 21:39:10 -0400

    .

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import pickle
 def load_data(file_path):
     """ Loads the data from a pickled file located at the given file_path. """
     with open(file_path, 'rb') as f:
-        u = pickle._Unpickler(f)
+        u = pickle.Unpickler(f)
         u.encoding = 'latin1'
         data = u.load()
     return data
 
-
 def generate_data():
-    path = '../cifar-10-batches-py'
+    path = 'cifar-10-batches-py'
     data = load_data(path + '/data_batch_1')
     x = data['data']
     y = data['labels']
@@ -26,12 +25,43 @@ def generate_data():
     idx = np.in1d(y, label_list)
     y = y[idx]
     x = x[idx]
-    return x, y
+
+    # split the data, to train, test
+    x_train = x[:12000]
+    y_train = y[:12000]
+    x_split = x[12000:]
+    y_split = y[12000:]
+    label_seen = [3, 5]
+    idx = np.in1d(y_train, label_seen)
+    x_train = x_train[idx]
+    y_train = y_train[idx]
+
+    idx = np.in1d(y_split, label_seen)
+    x_valid = x_split[idx]
+    y_valid = y_split[idx]
+
+    idx = np.in1d(y_split, [6])
+    x_test_unseen = x_split[idx]
+    y_test_unseen = y_split[idx]
+    return x_train, y_train, x_valid, y_valid, x_test_unseen, y_test_unseen
+
 
 
 def main():
-    x_train, y_train = generate_data()
-    print(x_train.shape)
+    x_train, y_train, x_valid, y_valid, x_test_unseen, y_test_unseen = generate_data()
+    '''
+    print x_train.shape
+    print "==============="
+    print y_train.shape
+    print "==============="
+    print x_valid.shape
+    print "==============="
+    print y_valid.shape
+    print "==============="
+    print x_test_unseen.shape
+    print "==============="
+    '''
+    
     pass
 
 
